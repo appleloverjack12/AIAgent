7384c203354a73dfe859a342411b3615d2bdd7a8
Jakov Smolić <jakovs2003@gmail.com>
2026-02-28 21:48:54 +0100

    Clean project: remove test files and unused code, ready for production

---
+++ src/types/elizaos.d.ts
@@ -0,0 +1,7 @@
+declare module '@elizaos/core' {
+  export const logger: any;
+  export type IAgentRuntime = any;
+  export type Character = any;
+  export type Project = any;
+  export type ProjectAgent = any;
+}
